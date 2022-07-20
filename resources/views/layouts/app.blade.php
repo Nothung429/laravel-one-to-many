c3935a682fa02fd8f7d3bf76fd72aa476a5cf186
Nothung429 <frank.gatto96.fg@gmail.com>
2022-07-20 17:05:59 +0200

    add category route

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -39,6 +39,12 @@
                         <li class="nav-item">
                             <a class="nav-link" href="{{route('admin.posts.create')}}">Create Post</a>
                         </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{route('admin.categories.index')}}">All Categories</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{route('admin.categories.create')}}">Create Category</a>
+                        </li>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
